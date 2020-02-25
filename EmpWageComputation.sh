f0c1cfab6905ba0d20500a5044d008eaf168920f
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-25 12:25:32 +0530

    MODIFIED : Calculate daily employee wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 isPresent=1
+PerHour=20
+FullDay=8
 random=$((RANDOM%2+1))
 if [ $isPresent -eq $random ] 
 then
 echo "Employee is Present "
+calculateDailyEmpWage=$(($PerHour*$FullDay))
+echo "Daily wage :$calculateDailyEmpWage"
 else
 echo "Employee is Absent "
 fi
