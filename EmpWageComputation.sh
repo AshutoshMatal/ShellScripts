ce71e1495bc22327328b6566abebfcef5c70f832
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-25 16:08:12 +0530

    MODIFIED :Calculating wages for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,26 +1,33 @@
-#!/bin/bash -x
+#/bin/bash -x
 echo "Welcome to Employee Wage Computation"
 isPresent=1
 PerHour=20
 FullDay=8
 PartTime=4
+numWorkingDays=20
+totalSalary=0
 randomCheck=$((RANDOM%3+1))
-# USING THE CASE  CALCULATE  FULL TIME AND PART TIME 
-case $randomCheck in
+
+# USING THE CASE  CALCULATE  FULL TIME AND PART TIME
+for (( day=1; day<=numWorkingDays; day++ ))
+do
+	randomCheck=$((RANDOM%3+1)) 
+			case $randomCheck in
 						# CALCULATING  FOR THE  FULL DAY
 				1)
 				 		calculateDailyEmpWage=$(($PerHour*$FullDay))
-	  			 		echo "Daily wage full day :$calculateDailyEmpWage"
 						;;
 						# CALCULATING FOR THE FULL TIME 
 		      2)
       				calculateDailyEmpWage=$(($PerHour*$PartTime))
-						echo "salary of part Time :$calculateDailyEmpWage"
 						;;
 			   3)
  						empHrs=0
 						salary=$(( $empHrs * $PerHour ))
 						;;
-esac
-
+			esac
+done
+ echo "Daily wage full day :$calculateDailyEmpWage"
+   echo "salary of part Time :$calculateDailyEmpWage"
+   totalSalary=$(($totalSalary + $calculateDailyEmpWage));
 
