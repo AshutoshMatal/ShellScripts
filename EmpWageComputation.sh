ca641b0b66e6df4ba357d58f6b1e23c8aa444933
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-25 15:29:49 +0530

    MODIFIED:Added in switch case

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,23 +4,23 @@ isPresent=1
 PerHour=20
 FullDay=8
 PartTime=4
-random=$((RANDOM%2+1))
-if [ $isPresent -eq $random ] 
-then
-	echo "Employee is Present " 
-	randomCheck=$((RANDOM%2+1)) 
-			if [ 1 -eq   $randomCheck ]
-			then
-   	 		 calculateDailyEmpWage=$(($PerHour*$FullDay))
-	  			 echo "Daily wage full day :$calculateDailyEmpWage" 
-			elif [ 2 -eq $randomCheck ]
-			then
-      		calculateDailyEmpWage=$(($PerHour*$PartTime))
-				echo "salary of part Time :$calculateDailyEmpWage"
-			fi
-else
-	echo "Employee is Absent "
- 	empHrs=0
-	salary=$(( $empHrs * $PerHour ))
-fi
+randomCheck=$((RANDOM%3+1))
+# USING THE CASE  CALCULATE  FULL TIME AND PART TIME 
+case $randomCheck in
+						# CALCULATING  FOR THE  FULL DAY
+				1)
+				 		calculateDailyEmpWage=$(($PerHour*$FullDay))
+	  			 		echo "Daily wage full day :$calculateDailyEmpWage"
+						;;
+						# CALCULATING FOR THE FULL TIME 
+		      2)
+      				calculateDailyEmpWage=$(($PerHour*$PartTime))
+						echo "salary of part Time :$calculateDailyEmpWage"
+						;;
+			   3)
+ 						empHrs=0
+						salary=$(( $empHrs * $PerHour ))
+						;;
+esac
+
 
