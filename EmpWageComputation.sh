911f13acb9eda170bfd6d7511459cddd726cb72a
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-25 19:00:59 +0530

    REFACTORED : Code is return in the function to get work hours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,37 +6,38 @@ PerHour=20
 FullDay=8
 PartTime=4
 totalWorkingDays=0
-totalEmpHr=0
+totalWorkHours=0
 #CONSTANTS
-MAX_HRS_IN_MONTH=100
+MAX_HRS_IN_MONTH=4
 PERHOUR=20
 NUM_WORKING_DAYS=20
-	# CALCULATE BY USING WHILE LOOP 
-while [[ totalEmpHr -lt  $MAX_HRS_IN_MONTH && totalWorkingDays -lt $NUM_WORKING_DAYS  ]]
-do
-	((totalWorkingDays++))
-		randomCheck=$((RANDOM%3+1)) 
+	# CALCULATE BY USING  FUNCTION
+function getWorkingHours()	
+{ 	    randomCheck=$1
 			case $randomCheck in
 						# CALCULATING  FOR THE  FULL DAY
 				1)
-				 		calculateDailyEmpWage=$(($PERHOUR * $FullDay))
-						empHrs=8
+						WorkHrs=8
 						;;
-						# CALCULATING FOR THE PART TIME 
-		      2)
-      				calculateDailyEmpWage1=$(($PERHOUR * $PartTime))
-						empHrs=4
+						# CALCULATING FOR THE PART TIME
+			   2)
+						WorkHrs=4
 						;;
 			   3)
- 						empHrs=0
+ 						WorkHrs=0
 						;;
 			esac
-#FINDING  THE  TOTAL EMPLOYEE HOURS
-				totalEmpHr=$(( $totalEmpHr + $empHrs ))
+		#CALCULATING  WORKING HOURS
+			echo "$WorkHrs"
+}
 
+while [[ totalWorkHours -lt  $MAX_HRS_IN_MONTH && totalWorkingDays -lt $NUM_WORKING_DAYS  ]]
+do
+   ((totalWorkingDays++))
+				WorkHrs="$( getWorkingHours $((RANDOM%3+1)) )"
+			#FINDING  THE  TOTAL EMPLOYEE HOURS
+				totalWorkHours=$(($totalWorkHours + $WorkHrs ))
 done
-   echo "Daily wage full day :$calculateDailyEmpWage"
-   echo "Daily wage of part Time :$calculateDailyEmpWage1"
-   totalSalary=$(($totalEmpHr * $PERHOUR));
-	echo "Total hours :$totalEmpHr"
+   totalSalary=$(($totalWorkHours * $PERHOUR));
+	echo "Total hours :$totalWorkHours"
 	echo "Total salary :$totalSalary"
