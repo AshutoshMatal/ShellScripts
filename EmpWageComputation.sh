ec6c878721e5cfc6b330dedbaf116fd7b529e17b
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-25 17:43:32 +0530

    MODIFIED : calculating 100 hours and 20 days salary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,33 +1,42 @@
 #/bin/bash -x
 echo "Welcome to Employee Wage Computation"
+#VARIABLES
 isPresent=1
 PerHour=20
 FullDay=8
 PartTime=4
-numWorkingDays=20
-totalSalary=0
-randomCheck=$((RANDOM%3+1))
-
-# USING THE CASE  CALCULATE  FULL TIME AND PART TIME
-for (( day=1; day<=numWorkingDays; day++ ))
+totalWorkingDays=0
+totalEmpHr=0
+#CONSTANTS
+MAX_HRS_IN_MONTH=100
+PERHOUR=20
+NUM_WORKING_DAYS=20
+	# CALCULATE BY USING WHILE LOOP 
+while [[ totalEmpHr -lt  $MAX_HRS_IN_MONTH && totalWorkingDays -lt $NUM_WORKING_DAYS  ]]
 do
-	randomCheck=$((RANDOM%3+1)) 
+	((totalWorkingDays++))
+		randomCheck=$((RANDOM%3+1)) 
 			case $randomCheck in
 						# CALCULATING  FOR THE  FULL DAY
 				1)
-				 		calculateDailyEmpWage=$(($PerHour*$FullDay))
+				 		calculateDailyEmpWage=$(($PERHOUR * $FullDay))
+						empHrs=8
 						;;
-						# CALCULATING FOR THE FULL TIME 
+						# CALCULATING FOR THE PART TIME 
 		      2)
-      				calculateDailyEmpWage=$(($PerHour*$PartTime))
+      				calculateDailyEmpWage1=$(($PERHOUR * $PartTime))
+						empHrs=4
 						;;
 			   3)
  						empHrs=0
-						salary=$(( $empHrs * $PerHour ))
 						;;
 			esac
-done
- echo "Daily wage full day :$calculateDailyEmpWage"
-   echo "salary of part Time :$calculateDailyEmpWage"
-   totalSalary=$(($totalSalary + $calculateDailyEmpWage));
+#FINDING  THE  TOTAL EMPLOYEE HOURS
+				totalEmpHr=$(( $totalEmpHr + $empHrs ))
 
+done
+   echo "Daily wage full day :$calculateDailyEmpWage"
+   echo "Daily wage of part Time :$calculateDailyEmpWage1"
+   totalSalary=$(($totalEmpHr * $PERHOUR));
+	echo "Total hours :$totalEmpHr"
+	echo "Total salary :$totalSalary"
